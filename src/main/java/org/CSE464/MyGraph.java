19e9a76df6fd9069e25dc37428fa43ecddadf5e4
itsJohnny21 <jsalazar6421@gmail.com>
2024-09-21 13:01:17 -0700

    Transition from JGraphT to Graphviz-Java.

--- src/main/java/org/CSE464/MyGraph.java
+++ src/main/java/org/CSE464/MyGraph.java
@@ -1,62 +1,49 @@
 package org.CSE464;
+import guru.nidi.graphviz.attribute.*;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Link;
+import guru.nidi.graphviz.model.MutableGraph;
+import guru.nidi.graphviz.model.MutableNode;
+import guru.nidi.graphviz.parse.Parser;
 
-import org.jgrapht.graph.*;
-import org.jgrapht.nio.DefaultAttribute;
-import org.jgrapht.nio.dot.DOTExporter;
-import org.jgrapht.nio.dot.DOTImporter;
-import org.jgrapht.nio.ImportException;
-import org.jgrapht.nio.Attribute;
-
-
+import javax.annotation.Nullable;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.io.InputStream;
+import java.util.LinkedHashSet;
+import java.util.List;
 
-public class MyGraph extends DirectedPseudograph<String, DefaultEdge> {
-    private final HashMap<String, HashMap<String, String>> attributes = new HashMap<>();
+public class MyGraph extends MutableGraph {
 
-    public MyGraph(Class edgeClass) {
-        super(edgeClass);
+    protected MyGraph(boolean strict, boolean directed, boolean cluster, Label name, LinkedHashSet<MutableNode> nodes, LinkedHashSet<MutableGraph> subgraphs, List<Link> links, @Nullable Attributes<? extends ForNode> nodeAttrs, @Nullable Attributes<? extends ForLink> linkAttrs, @Nullable Attributes<? extends ForGraph> graphAttrs) {
+        super(strict, directed, cluster, name, nodes, subgraphs, links, nodeAttrs, linkAttrs, graphAttrs);
     }
 
-    private static MyGraph parseGraph(String filepath) {
-        MyGraph graph = new MyGraph(DefaultEdge.class);
+    public static MyGraph fromMutableGraph(MutableGraph g) {
+        return new MyGraph(g.isStrict(), g.isDirected(), g.isCluster(), g.name(), new LinkedHashSet<>(g.nodes()), new LinkedHashSet<>(g.graphs()), g.links(), g.nodeAttrs(), g.linkAttrs(), g.graphAttrs());
+    }
 
-        DOTImporter<String, DefaultEdge> dotImporter = new DOTImporter<>();
-        dotImporter.setVertexFactory(name -> name);
-        dotImporter.addVertexAttributeConsumer((pair, attr) -> {
-            String vertex = pair.getFirst();
-            String attributeName = pair.getSecond();
-            String attributeValue = attr.getValue();
-
-            if (!graph.attributes.containsKey(vertex)) {
-                graph.attributes.put(vertex, new HashMap<String, String>());
-            }
-
-            graph.attributes.get(vertex).put(attributeName, attributeValue);
-        });
-
-        try (Reader dotFileReader = new FileReader(filepath)) {
-            dotImporter.importGraph(graph, dotFileReader);
-        } catch (ImportException | IOException e) {
-            e.printStackTrace();
+    public static MyGraph parseGraph (String filepath) {
+        try (InputStream dot = MyGraph.class.getResourceAsStream(filepath)) {
+            assert dot != null;
+            MyGraph g = fromMutableGraph(new Parser().read(new File(filepath)));
+            Graphviz.fromGraph(g).width(700).render(Format.PNG).toFile(new File("trash/ex4-1.png"));
+            return  g;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        return graph;
     }
 
     public String edgeDirections() {
-        if (this.edgeSet().size() == 0) {
+        if (this.edges().size() == 0) {
             return "";
         }
 
         StringBuilder sb = new StringBuilder("[");
-        for (DefaultEdge edge : this.edgeSet()) {
-            sb.append(this.getEdgeSource(edge)).append(" -> ").append(this.getEdgeTarget(edge)).append(", ");
+        for (Link edge : this.edges()) {
+            assert edge.from() != null;
+            sb.append(edge.from().name()).append(" -> ").append(edge.to().name()).append(", ");
         }
 
         sb.replace(sb.length() - 2, sb.length() , "");
@@ -64,78 +51,34 @@ public class MyGraph extends DirectedPseudograph<String, DefaultEdge> {
         return sb.toString();
     }
 
-    public String nodeLabels() {
-        StringBuilder sb = new StringBuilder();
-        String longestNodeName = this.vertexSet()
-                .stream()
-                .max((node1, node2) -> Integer.compare(node1.length(), node2.length()))
-                .orElse(null);
-
-        String longestNodeLabel = nodeLabel(this.vertexSet()
-                .stream()
-                .max((node1, node2) -> Integer.compare(nodeLabel(node1).length(), nodeLabel(node2).length()))
-                .orElse(null));
-
-        int nameWidth = Math.max("Node".length(), longestNodeName.length()) + 3;
-        int labelWidth = Math.max("Label".length(), longestNodeLabel.length()) + 3;
-
-
-
-        sb.append(String.format("%-" + nameWidth + "s | %-" + labelWidth + "s\n", "Node", "Label"));
-        sb.append("—".repeat(nameWidth + labelWidth)).append("\n");
-
-        for (String node : this.vertexSet()) {
-            sb.append(String.format("%-" + nameWidth + "s | %-" + labelWidth + "s\n", node, nodeLabel(node)));
-        }
-
-        return sb.substring(0, sb.length() - 1);
-    }
-
-    public String nodeLabel(String node) {
-        if (this.attributes.containsKey(node) && this.attributes.get(node).containsKey("label")) {
-            return this.attributes.get(node).get("label");
-        } else {
-            return "";
-        }
-    }
-
-    public static void outputGraph(MyGraph graph, String filepath) {
-        DOTExporter<String, DefaultEdge> dotExporter = new DOTExporter<>();
-
-        dotExporter.setVertexIdProvider(name -> name);
-
-        dotExporter.setVertexAttributeProvider(name -> {
-            Map<String, Attribute> attributes = new LinkedHashMap<>();
-
-            if (!graph.nodeLabel(name).isEmpty()) {
-                attributes.put("label", DefaultAttribute.createAttribute(graph.nodeLabel(name)));
-            }
-            return attributes;
-        });
-
-        dotExporter.exportGraph(graph, new File(filepath));
-    }
-
-    @Override
-    public String toString() {
-        return
-                "Number of nodes: " + this.vertexSet().size()
-                +"\n" + nodeLabels()
-                +"\nNumber of edges: " + this.edgeSet().size()
-                +"\nNodes: " + this.vertexSet()
-                +"\nEdge Directions: " + this.edgeDirections()
-                ;
-    }
-
     public static void main(String[] args) {
-        MyGraph graph = MyGraph.parseGraph("/Users/jonisalazar/School/Fall 2024/CSE464/CSE-464-2024-jsalaz59/src/main/resources/graph1.dot");
-        System.out.println(graph);
-        MyGraph.outputGraph(graph, "/Users/jonisalazar/School/Fall 2024/CSE464/CSE-464-2024-jsalaz59/src/main/resources/graph1-output.dot");
-        System.out.println("Woohoo🥳😀");
+        MyGraph g = MyGraph.parseGraph("/Users/jonisalazar/School/Fall 2024/CSE464/CSE-464-2024-jsalaz59/src/main/resources/graph1.dot");
+        System.out.print(
+                "Number of nodes: " + g.nodes().size()
+                +"\nNodes: " + g.nodes()
+                +"\nNumber of edges: " + g.edges().size()
+                +"\nEdge Directions: "  + g.edgeDirections()
+                );
+//        System.out.println(g.name());
+//        System.out.println(g.nodes().size());
+//
+//        g.edges().forEach(e -> {
+//            assert e.from() != null;
+//            System.out.printf("%s -> %s%n", e.from().name(), e.to().name());
+//        });
+
     }
 }
 
-// TODO: JUnit testing to guarantee correctness.
-// TODO: Read JGraphtT documentation to understand how the DOTParser and Graph interface works.
-// TODO: Complete README.md file.
-// TODO: Ask instructor if using JGraphT this is way is allowed.
+/*
+Number of nodes: 3
+Node    | Label
+—————————————————
+a       | apple
+b       | banana
+c       | coconut
+Number of edges: 4
+Nodes: [a, b, c]
+Edge Directions: [a -> b, b -> b, b -> c, c -> a]
+Woohoo🥳😀
+ */
