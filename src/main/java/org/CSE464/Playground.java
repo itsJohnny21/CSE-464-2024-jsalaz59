100bd5baccbea78a14e180bb11511efbc8d6eaf6
itsJohnny21 <jsalazar6421@gmail.com>
2024-09-23 21:43:07 -0700

    Ignore.

--- src/main/java/org/CSE464/Playground.java
+++ src/main/java/org/CSE464/Playground.java
@@ -1,26 +1,33 @@
-package org.CSE464;
+// package org.CSE464;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-public class Playground {
-    public static void main(String[] args) {
-        MyGraph g = MyGraph.parseGraph(
-                "/Users/jonisalazar/School/Fall 2024/CSE464/CSE-464-2024-jsalaz59/src/test/resources/DOT/valid/funnyGraph/funnyGraph.dot");
-        // System.err.println(g.isDirected());
-        // System.out.println(g);
-        // System.out.println(g.getEdgeDirections());
-        System.out.println(g);
-        // Set<String> edges = g.edges().stream().map(e -> e.name().toString()).collect(Collectors.toSet());
-        // System.out.printf("edges: %s\n", edges);
-        // System.out.println(g.getNodeLabels());
-        // System.out.println(g.getEdgeDirections());
+// public class Playground {
+//     public static void main(String[] args) {
+//         MyGraph g = MyGraph.parseGraph(
+//                 "/Users/jonisalazar/School/Fall 2024/CSE464/CSE-464-2024-jsalaz59/src/test/resources/DOT/valid/funnyGraph/funnyGraph.dot");
+//         // System.err.println(g.isDirected());
+//         // System.out.println(g);
+//         // System.out.println(g.getEdgeDirections());
+//         System.out.println(g);
+//         try {
+//             g.outputGraph(
+//                     "/Users/jonisalazar/School/Fall 2024/CSE464/CSE-464-2024-jsalaz59/src/test/reso~`{}. urces/tmp/tmp");
 
-        try {
-            MyGraph.outputGraph(g,
-                    "/Users/jonisalazar/School/Fall 2024/CSE464/CSE-464-2024-jsalaz59/src/test/resources/tmp/dr-10.png");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Woohoo🥳😀");
-    }
-}
+//         } catch (Exception e) {
+//             System.out.println("EXCEPTION");
+//         }
+//         // Set<String> edges = g.edges().stream().map(e -> e.name().toString()).collect(Collectors.toSet());
+//         // System.out.printf("edges: %s\n", edges);
+//         // System.out.println(g.getNodeLabels());
+//         // System.out.println(g.getEdgeDirections());
+
+//         try {
+//             MyGraph.outputGraph(g,
+//                     "/Users/jonisalazar/School/Fall 2024/CSE464/CSE-464-2024-jsalaz59/src/test/resources/tmp/dr-10.png");
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+//         System.out.println("Woohoo🥳😀");
+//     }
+// }
