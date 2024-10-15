e65c8cecf6e5a42a427a88c340875a4938fc2fe6
Jonathan Salazar <jsalazar6421@gmail.com>
2024-10-14 22:24:42 -0700

    Change: Exception implemented.

--- src/main/java/org/CSE464/DifferingGraphsException.java
+++ src/main/java/org/CSE464/DifferingGraphsException.java
@@ -1,5 +1,11 @@
 package org.CSE464;
 
-public class DifferingGraphsException {
+public class DifferingGraphsException extends RuntimeException {
+    public DifferingGraphsException() {
+        super("Graphs differ.");
+    }
 
+    public DifferingGraphsException(String message) {
+        super(message);
+    }
 }
